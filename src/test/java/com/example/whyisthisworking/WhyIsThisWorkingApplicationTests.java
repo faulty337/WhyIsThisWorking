4385b0ff90c61d7f7fcd894cdb15cba1bd2c5bad
faulty337 <faulty337@gmail.com>
2023-12-24 17:41:38 +0900

    Feat : 테스트 환경 Profile 추가

--- src/test/java/com/example/whyisthisworking/WhyIsThisWorkingApplicationTests.java
+++ src/test/java/com/example/whyisthisworking/WhyIsThisWorkingApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.whyisthisworking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WhyIsThisWorkingApplicationTests {
 
     @Test
