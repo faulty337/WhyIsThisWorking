18be6d168caf191107fa8b6990df6bbd0d3ada79
faulty337 <faulty337@gmail.com>
2023-12-24 17:40:47 +0900

    Feat : add swagger

--- src/main/java/com/example/whyisthisworking/common/config/SwaggerConfig.java
+++ src/main/java/com/example/whyisthisworking/common/config/SwaggerConfig.java
@@ -1,21 +1,28 @@
 package com.example.whyisthisworking.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
+@Profile("!test")
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
                 .info(apiInfo());
+    }
+
+    @Bean
+    public GroupedOpenApi customGroupOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("custom-group")
+                .pathsToMatch("/api/example/**") // 여기에서 경로를 설정합니다.
+                .build();
     }
 
     private Info apiInfo() {
